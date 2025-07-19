54ab20a2fade083e34b8f2e89949cb0bd9c19693
NathanProfirio <gooyjoker1179@gmail.com>
2025-07-18 22:52:09 -0300

    primeiro execicio de logica de programação python

---
+++ aula_de_operadores_aritméticos/atv1_antecessor_e_sucessor.py
@@ -0,0 +1,4 @@
+numero = int (input('digite um numero para saber seu antecessor e seu sucessor:'))
+antes = (numero - 1)
+depois = (numero + 1)
+print('O numero que voce escolheu e o {} seu antecesso e {} e seu sucessor e {}'.format(numero, antes, depois))
